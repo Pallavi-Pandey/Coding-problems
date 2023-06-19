a0e391e6e543a7a174b827c8d1c767a97cce497c
Pallavi Pandey <96332940+Pallavi-IITM-2021@users.noreply.github.com>
2023-06-19 12:14:21 +0530

    Update 18_june.py

--- 18_june.py
+++ 18_june.py
@@ -5,29 +5,14 @@
 # Solution:
 class Solution:
     def reverse(self, x: int) -> int:
-        # Check if the given number is negative
         is_negative = True if x < 0 else False
-        
-        # Convert the number to a positive value for reversal
-        x = abs(x)
-        
-        # Initialize the reversed number
+        x = abs(x
         reversed_num = 0
-        
         while x > 0:
-            # Extract the last digit of the number
             digit = x % 10
-            
-            # Append the digit to the reversed number
             reversed_num = reversed_num * 10 + digit
-            
-            # Remove the last digit from the number
             x //= 10
-        
-        # Apply the sign to the reversed number
         reversed_num = -reversed_num if is_negative else reversed_num
-        
-        # Check for integer overflow
         if reversed_num < -2**31 or reversed_num > 2**31 - 1:
             return 0
         
