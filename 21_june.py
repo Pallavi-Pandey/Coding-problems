9da40bcffc7ef60b178362cf2c17604bb175aa70
Pallavi Pandey <96332940+Pallavi-IITM-2021@users.noreply.github.com>
2023-06-22 23:11:57 +0530

    21_june.py

---
+++ 21_june.py
@@ -0,0 +1,21 @@
+You are given an array of characters letters that is sorted in non-decreasing order, and a character target. 
+There are at least two different characters in letters.
+Return the smallest character in letters that is lexicographically greater than target. 
+If such a character does not exist, return the first character in letters.
+
+Solution:
+from typing import List
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        left, right = 0, len(letters) - 1
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            
+            if letters[mid] <= target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return letters[left % len(letters)]
